ae7142c2a9e77824a632d233d09eb6952b3d1a3d
Rishabh Malviya <rishabh.malviya@gmail.com>
2015-09-24 10:44:42 +0530

    First Successful Run

--- devel/share/localization/cmake/localization-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(localization_MESSAGE_FILES "/home/rishabh/localization/src/localization/msg/lp.msg;/home/rishabh/localization/src/localization/msg/lla.msg;/home/rishabh/localization/src/localization/msg/roboteq_msg.msg")
-set(localization_SERVICE_FILES "")
